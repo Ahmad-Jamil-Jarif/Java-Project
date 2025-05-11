cbfdd2336f67be9be17b0e5696fb1d3ae1ee42c0
Ahmad-Jamil-Jarif <jamilcse2027@gmail.com>
2025-05-11 15:20:46 +0600

    Add files via upload

---
+++ Disaster management/src/disaster/management/DisasterManagement.java
@@ -0,0 +1,17 @@
+
+package disaster.management;
+
+/**
+ *
+ * @author demon
+ */
+public class DisasterManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
